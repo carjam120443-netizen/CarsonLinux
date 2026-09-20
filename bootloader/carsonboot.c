7140725731befa0c78469b4d011ba75176841a45
carjam120443-netizen <carjam120443@gmail.com>
2026-09-20 12:05:41 -0400

    Make CarsonBoot menu timing more reliable

--- bootloader/carsonboot.c
+++ bootloader/carsonboot.c
@@ -16,25 +16,21 @@ static void print_banner(void) {
 }
 
 static void print_menu(UINTN selected, UINTN seconds_left) {
-    UINTN i;
-
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
     Print(L"  CarsonBoot\r\n\r\n");
 
-    for (i = 0; i < 2; i++) {
-        if (i == selected)
-            ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE | EFI_BACKGROUND_BLUE);
-        else
-            ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
-
-        if (i == 0)
-            Print(L"  %s Boot CarsonLinux\r\n", i == selected ? L">" : L" ");
-        else
-            Print(L"  %s Reboot to firmware\r\n", i == selected ? L">" : L" ");
-    }
-
+    if (selected == 0)
+        ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE | EFI_BACKGROUND_BLUE);
+    Print(L"  > Boot CarsonLinux\r\n");
     ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
-    Print(L"\r\n  Use Up/Down to select, Enter to boot.\r\n");
-    Print(L"  Auto-booting CarsonLinux in %u seconds...\r\n", seconds_left);
+
+    if (selected == 1)
+        ST->ConOut->SetAttribute(ST->ConOut, EFI_WHITE | EFI_BACKGROUND_BLUE);
+    Print(L"    Reboot to firmware\r\n");
+    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
+
+    Print(L"\r\n  Up/Down: select    Enter: boot\r\n");
+    Print(L"  Auto-boot in %u seconds...\r\n", seconds_left);
 }
 
 static EFI_STATUS launch_grub(void) {
@@ -53,8 +49,8 @@ static EFI_STATUS launch_grub(void) {
 
     status = uefi_call_wrapper(BS->LoadImage, 6, FALSE, carsonboot_image,
                                device_path, NULL, 0, &image);
-
     FreePool(device_path);
+
     if (EFI_ERROR(status)) {
         Print(L"\r\n[CarsonBoot] Could not load GRUB: %r\r\n", status);
         return status;
@@ -71,10 +67,9 @@ static EFI_STATUS boot_menu(void) {
     UINTN index;
     UINTN selected = 0;
     UINTN seconds_left = 5;
-    UINTN countdown_ticks = 0;
 
-    status = uefi_call_wrapper(BS->CreateEvent, 5, EVT_TIMER, 0, NULL, NULL,
-                               NULL, &timer_event);
+    status = uefi_call_wrapper(BS->CreateEvent, 5, EVT_TIMER, TPL_CALLBACK,
+                               NULL, NULL, NULL, &timer_event);
     if (EFI_ERROR(status))
         return status;
 
@@ -88,10 +83,11 @@ static EFI_STATUS boot_menu(void) {
     events[0] = ST->ConIn->WaitForKey;
     events[1] = timer_event;
 
-    ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
+    ST->ConOut->ClearScreen(ST->ConOut);
+    print_banner();
     print_menu(selected, seconds_left);
 
-    while (1) {
+    while (seconds_left > 0) {
         status = uefi_call_wrapper(BS->WaitForEvent, 3, 2, events, &index);
         if (EFI_ERROR(status))
             break;
@@ -102,33 +98,33 @@ static EFI_STATUS boot_menu(void) {
             if (EFI_ERROR(status))
                 continue;
 
-            if (key.ScanCode == SCAN_UP || key.ScanCode == SCAN_DOWN) {
-                selected = selected == 0 ? 1 : 0;
-                ST->ConOut->ClearScreen(ST->ConOut);
-                print_banner();
-                print_menu(selected, seconds_left);
+            if (key.ScanCode == SCAN_UP) {
+                selected = 0;
+            } else if (key.ScanCode == SCAN_DOWN) {
+                selected = 1;
             } else if (key.UnicodeChar == CHAR_CARRIAGE_RETURN) {
                 break;
+            } else {
+                continue;
             }
+
+            ST->ConOut->ClearScreen(ST->ConOut);
+            print_banner();
+            print_menu(selected, seconds_left);
         } else {
-            countdown_ticks++;
-            if (countdown_ticks >= 1) {
-                if (seconds_left > 0)
-                    seconds_left--;
+            if (seconds_left > 0)
+                seconds_left--;
 
-                countdown_ticks = 0;
+            ST->ConOut->ClearScreen(ST->ConOut);
+            print_banner();
 
-                ST->ConOut->ClearScreen(ST->ConOut);
-                print_banner();
-
-                if (seconds_left == 0) {
-                    selected = 0;
-                    Print(L"  Auto-booting CarsonLinux...\r\n\r\n");
-                    break;
-                }
-
-                print_menu(selected, seconds_left);
+            if (seconds_left == 0) {
+                selected = 0;
+                Print(L"  Auto-booting CarsonLinux...\r\n\r\n");
+                break;
             }
+
+            print_menu(selected, seconds_left);
         }
     }
 
@@ -147,6 +143,7 @@ static EFI_STATUS boot_menu(void) {
 
 EFI_STATUS EFIAPI efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *system_table) {
     EFI_STATUS status;
+
     carsonboot_image = image;
     ST = system_table;
     BS = ST->BootServices;
@@ -156,9 +153,10 @@ EFI_STATUS EFIAPI efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *system_table) {
     print_banner();
 
     status = boot_menu();
+
     if (EFI_ERROR(status)) {
         ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTRED | EFI_BACKGROUND_BLACK);
-        Print(L"  [!] Unable to start the CarsonLinux boot path.\r\n");
+        Print(L"  [!] CarsonBoot failed to start the boot path.\r\n");
         Print(L"  Status: %r\r\n", status);
         ST->ConOut->SetAttribute(ST->ConOut, EFI_LIGHTGRAY | EFI_BACKGROUND_BLACK);
         Print(L"\r\n  Press any key to return to firmware.\r\n");
